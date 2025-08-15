27f532185f613224e21c1189fb84b3ae09fa9c7b
rkajy <rado_andria@yahoo.com>
2025-08-15 11:48:12 +0200

    print_arg.c

--- test/Testft.c
+++ test/Testft.c
@@ -17,6 +17,31 @@ void test_plus_flag(void) {
     compare_printfs("2 -> |%+d|\n", -42);
 }
 
+// Test pour le flag espace
+void test_space_flag(void) {
+    compare_printfs("3 -> |% d|\n", 42);
+    compare_printfs("4 -> |% d|\n", -42);
+}
+
+// Test pour le flag 0
+void test_zero_flag(void) {
+    compare_printfs("5 -> |%05d|\n", 42);
+    compare_printfs("6 -> |%05d|\n", -42);
+}
+
+// Test pour la précision sur les nombres
+void test_precision_numbers(void) {
+    compare_printfs("9  -> |%.5d|\n", 42);
+    compare_printfs("10 -> |%.5d|\n", -42);
+    compare_printfs("11 -> |%.0d|\n", 0);
+}
+
+// Test pour la précision sur les chaînes
+void test_precision_strings(void) {
+    compare_printfs("12 -> |%.3s|\n", "Hello");
+    compare_printfs("13 -> |%.0s|\n", "Hello");
+}
+
 void setUp()
 {
 
@@ -31,6 +56,11 @@ int main(void)
 {
 	UNITY_BEGIN();
 	RUN_TEST(test_plus_flag);
+	RUN_TEST(test_space_flag);
+	RUN_TEST(test_zero_flag);
+	RUN_TEST(test_precision_numbers);
+	RUN_TEST(test_precision_strings);
+
 	UNITY_END();
 
 	return 0;
