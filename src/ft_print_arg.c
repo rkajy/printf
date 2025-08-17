eb70447186258cc5e65d5b14ffb93f44674bc686
rkajy <rado_andria@yahoo.com>
2025-08-17 16:24:16 +0200

    prep

--- src/ft_print_arg.c
+++ src/ft_print_arg.c
@@ -1,33 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   dispatcher.c                                       :+:      :+:    :+:   */
+/*   ft_print_arg.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: radandri <radandri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/15 11:10:02 by radandri          #+#    #+#             */
-/*   Updated: 2025/08/15 11:45:50 by radandri         ###   ########.fr       */
+/*   Updated: 2025/08/17 16:06:40 by radandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "printf.h"
+#include "ft_printf.h"
 
-int print_arg(t_fornat *fmt, va_list *args)
+int print_arg(t_format *fmt, va_list *args)
 {
     if(fmt->type == 'c')
-        return print_char(fmt, args);
+        print_char(fmt, args);
     else if(fmt->type == 's')
-        return print_string(fmt, args);
+        print_string(fmt, args);
     else if(fmt->type == 'd' || fmt->type == 'i')
-        return print_integer(fmt, args);
+        print_integer(fmt, args);
     else if(fmt->type == 'u')
-        return print_unsigned(fmt, args);
-    else if(fmt->type == x || fmt->type == "X")
-        return print_hexadecimal(fmt, args);
+        print_unsigned(fmt, args);
+    else if(fmt->type == 'x' || fmt->type == 'X')
+        print_hexadecimal(fmt, args);
     else if(fmt->type == 'p')
-        return print_pointer(fmt, args);
+        print_pointer(fmt, args);
     else if(fmt->type == '%')
-        return print_percent(fmt);
+        print_percent(fmt);
     else
         return -1; // Unsupported format type
+    return 0; // Successfully printed the argument
 }
--- src/ft_print_utils_numeric.c
+++ src/ft_print_utils_numeric.c
@@ -1,19 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   ft_print_utils_numeric.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: radandri <radandri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/09 20:26:42 by radandri          #+#    #+#             */
-/*   Updated: 2025/08/16 19:59:36 by radandri         ###   ########.fr       */
+/*   Created: 2025/08/15 12:45:40 by radandri          #+#    #+#             */
+/*   Updated: 2025/08/17 16:23:41 by radandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int ft_printf(const char *format, ...)
+
+void	print_integer(t_format *fmt, va_list *args)
 {
-    (void)format;
-    return 0;
+
+}
+void	print_unsigned(t_format *fmt, va_list *args)
+{
+
+}
+void	print_hexadecimal(t_format *fmt, va_list *args)
+{
+
+}
+void	print_pointer(t_format *fmt, va_list *args)
+{
+
 }
