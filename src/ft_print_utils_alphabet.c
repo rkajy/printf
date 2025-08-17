9f8085e9c02f857274cd3980241ecc7d9b722f83
Rado Andriamahakajy <94129893+rkajy@users.noreply.github.com>
2025-08-17 16:26:23 +0200

    prep (#15)
    decoupage fichiers et maj prototypes des fonctions

--- src/ft_print_utils_alphabet.c
+++ src/ft_print_utils_alphabet.c
@@ -1,19 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   ft_print_utils_alphabet.c                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: radandri <radandri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/09 20:26:42 by radandri          #+#    #+#             */
-/*   Updated: 2025/08/16 19:59:36 by radandri         ###   ########.fr       */
+/*   Created: 2025/08/15 13:43:38 by radandri          #+#    #+#             */
+/*   Updated: 2025/08/17 16:23:41 by radandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int ft_printf(const char *format, ...)
+void print_char(t_format *fmt, va_list *args)
 {
-    (void)format;
-    return 0;
+    
+}
+
+void print_string(t_format *fmt, va_list *args)
+{
+ 
+}
+void print_percent(t_format *fmt)
+{
+
 }
--- src/ft_printf.h
+++ src/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: radandri <radandri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/11 21:37:42 by radandri          #+#    #+#             */
-/*   Updated: 2025/08/15 11:45:47 by radandri         ###   ########.fr       */
+/*   Updated: 2025/08/17 16:23:31 by radandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,45 @@
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-# include <stddef.h>
-# include <stdarg.h>
+# include <stddef.h> // size_t
+# include <stdarg.h> // va_list, va_start, va_end
+# include <stdlib.h> // malloc, free
+# include <unistd.h> // write
 
 typedef struct s_format {
-	int flag_minus;
-	int flag_plus;
-	int flag_space;
-	int flag_zero;
-	int flag_hash;
-	int width;
-	int precision;
-	int length;
+	int flag_minus; // Left alignment, eg ft_printf("%-d", 42) should print "42 ", ft_printf("%-d", -42) should print "-42 ", ft_printf("%-5d", 42) should print "42   ", ft_printf("%-5d", -42) should print "-42  "
+	int flag_plus; // Force sign for positive numbers, eg ft_printf("%+d", 42) should print "+42", ft_printf("%+d", -42) should print "-42"
+	int flag_space; // Space before positive numbers, eg ft_printf("% d", 42) should print " 42", ft_printf("% d", -42) should print "-42"
+	int flag_zero; // Zero padding, eg ft_printf("%05d", 42) should print "00042", ft_printf("%05d", -42) should print "-0042"
+	int flag_hash; // Alternate form for hexadecimal and octal, eg ft_printf("%#x", 255) should print "0xff"
+	char type; // Format specifier (c, s, d, i, u, x, X, p, %)
+	int width; // Minimum field width
+	int precision; // Precision for floating-point numbers or string truncation, eg ft_printf("%.2f", 3.14159) should print "3.14", ft_printf("%.5s", "Hello") should print "Hello", 
+	// ft_printf("%10s", "Hi") should print "        Hi", ft_printf("%-10s", "Hi") should print "Hi        "
+	// ft_printf("%5d", 42) should print "   42", ft_printf("%-5d", 42) should print "42   ", ft_printf("%05d", 42) should print "00042"
+	// ft_printf("%5.2f", 3.14159) should print " 3.14", ft_printf("%-5.2f", 3.14159) should print "3.14 "
+	// ft_printf("%5.2s", "Hello") should print "He   ", ft_printf("%-5.2s", "Hello") should print "He   "
+	// ft_printf("%5c", 'A') should print "    A", ft_printf("%-5c", 'A') should print "A    "
+	// ft_printf("%.5d", 42) should print "00042", ft_printf("%.5d", -42) should print "-0042"
+	// ft_printf(".0d", 0) should print "", ft_printf(".0d", 42) should print "42", ft_printf(".0d", -42) should print "-42"
+	// ft_print("%.3s", "Hello") should print "Hel", ft_printf("%.3s", NULL) should print "(null)"
+	// ft_printf("%.0s", "Hello") should print "", ft_printf("%.0s", NULL) should print ""
 } t_format;
 
+// Function from libft
+int	ft_int_len(long n);
+char *ft_itoa(int n);
 
+// Function prototypes for ft_printf
 int     ft_printf(const char *format, ...);
-int print_arg(t_fornat *fmt, va_list *args);
-int print_char(t_format *fmt, va_list *args);
-int print_string(t_format *fmt, va_list *args);
-int print_integer(t_format *fmt, va_list *args);
-int print_unsigned(t_format *fmt, va_list *args);
-int print_hexadecimal(t_format *fmt, va_list *args);
-int print_pointer(t_format *fmt, va_list *args);
-int print_percent(t_format *fmt);
-int parse_format(const char **format, t_format *fmt);
-int parse_flags(const char **format, t_format *fmt);
+int print_arg(t_format *fmt, va_list *args);
+void print_char(t_format *fmt, va_list *args);
+void print_string(t_format *fmt, va_list *args);
+void print_integer(t_format *fmt, va_list *args);
+void print_unsigned(t_format *fmt, va_list *args);
+void print_hexadecimal(t_format *fmt, va_list *args);
+void print_pointer(t_format *fmt, va_list *args);
+void print_percent(t_format *fmt);
+const char *parse_flags(const char *format, t_format *fmt);
 
 #endif // FT_PRINTF_H
